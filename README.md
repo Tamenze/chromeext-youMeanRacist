6386ca06880ab2250d37e795d7278aafc80a75ae
Tolulope Edionwe <tamenze@gmail.com>
2019-01-27 17:16:37 -0500

    mmkay

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a Chrome extension that allows the user to correct coded language often used by publications to soften the impact of racial discrimination.
 
-"Racially charged" is corrected to read "Racist" with a click of a button. 
+"Racially charged" or "Racially tinged"  is corrected to read "Racist" with a click of a button. 
--- content.js
+++ content.js
@@ -1,27 +1,30 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
-	console.log('something happening from the extension');
+	// console.log('something happening from the extension');
 
 	replaceTextOnPage();
 
 		function replaceTextOnPage(){
 		  	getAllTextNodes().forEach(function(node){
-					if 
-					(
-						node.nodeValue.toLowerCase().includes("racially charged") || 
-						node.nodeValue.includes("racially-charged") ||
-						node.nodeValue.toLowerCase().includes("racially tinged") ||
-						node.nodeValue.toLowerCase().includes("racially-tinged")
-					)
+						if (
+							node.nodeValue.toLowerCase().includes("racially charged") ||
+							node.nodeValue.toLowerCase().includes("racially-charged") ||
+							node.nodeValue.toLowerCase().includes("racially tinged") ||
+							node.nodeValue.toLowerCase().includes("racially-tinged")
+						)
 			  	{
-			  		//replace and match case
-			  		node.nodeValue = node.nodeValue.replace(/racially charged|racially-charged/,'racist');
-						node.nodeValue = node.nodeValue.replace(/Racially Charged|Racially-Charged| Racially charged| Racially-charged/,'Racist');
-						node.nodeValue = node.nodeValue.replace(/racially tinged|racially-tinged/, 'racist');
-						node.nodeValue = node.nodeValue.replace(/Racially Tinged|Racially-Tinged| Racially tinged| Racially-tinged/, 'Racist');
+						// replace and match case
+						node.nodeValue = node.nodeValue.replace(/racially charged|racially-charged|racially tinged|racially-tinged/,'racist');
+						node.nodeValue = node.nodeValue.replace(/Racially Charged|Racially-Charged| Racially charged| Racially-charged |Racially Tinged|Racially-Tinged| Racially tinged| Racially-tinged/,'Racist');
+						// issue is that when there is more than one mention of the above phrase in a text node, 
+						// attempt1: if there's a way to check whether or not the nodevalue includes the requested term, i could wrap the above in a thing that repeats, so while node.nodevalue includes Blah, run this replace code. when it returns false, move on to the styling code.
+
+						// need to make a promise in order to have the access to .then()
 
 					
-					//make and attach new text node structure with additional styled span
-					var index1 = (node.nodeValue.indexOf("racist") !== -1) ? node.nodeValue.indexOf("racist") : node.nodeValue.indexOf("Racist")
+					// make and attach new text node structure with additional styled span
+					var index1 = (node.nodeValue.indexOf("racist") !== -1) ? node.nodeValue.indexOf("racist") : node.nodeValue.indexOf("Racist");
+					//issue is this catches "racist" mentions that pre-exist the newly replaced one. 
+					//solution: made initial change racist*, so that second comb catches only recently changed ones
 					var index2 = index1 + 6;
 					var parentNode = node.parentNode;
 					
